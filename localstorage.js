14b72d19a434ac2412eb051cc97f1e207c2f064a
Agustin Hofmann <agustinhofmann@gmail.com>
2024-08-02 15:43:24 -0300

    Empece un modelo para entender la  base de datos

---
+++ localstorage.js
@@ -0,0 +1,14 @@
+function obtener_localstorage
+
+//function guardar_localstorage(){
+
+let persona = {
+        nombre: "agus",
+        edad: 31,
+        correo: "xyz@xyz.com",
+    }
+}
+
+let nombre = "agus"
+
+localStorage.setItem ("nombre",JSON.stringify(nombre));
